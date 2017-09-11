2c6615ae1ff75c49e0866133f46af8da4c70671c
He Jun <freefcw@gmail.com>
2017-09-11 23:52:53 +0800

    feat(config): app.name can config in the .env file

--- config/app.php
+++ config/app.php
@@ -14,7 +14,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'hustoj',
+    'name' => env('APP_NAME', 'hustoj'),
 
     /*
     |--------------------------------------------------------------------------
