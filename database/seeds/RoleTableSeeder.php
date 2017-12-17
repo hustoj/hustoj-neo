26fc710821716bccb8e04d233dc1eb28a7d94c50
He Jun <freefcw@gmail.com>
2017-12-17 22:27:48 +0800

    feat(db):add role seed

---
+++ database/seeds/RoleTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Entities\Role;
+use Illuminate\Database\Seeder;
+
+class RoleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role = new Role();
+        $role->name = 'admin';
+        $role->display_name = 'Administrator';
+        $role->save();
+    }
+}
