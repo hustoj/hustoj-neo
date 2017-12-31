efed4e784062fd8000ccb27c172e4de471eaeae2
He Jun <freefcw@gmail.com>
2018-01-01 00:13:39 +0800

    feat(home): home page

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -5,27 +5,22 @@
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
 			<div class="panel panel-default">
-				<div class="panel-heading">Home</div>
+				<div class="panel-heading">Welcome to HUSTOJ!</div>
 
-				<div class="panel-body">
-					<div class="jumbotron">
-						Welcome to HUSTOJ!
-					</div>
-					<div class="news">
-						@foreach($news as $post)
-						<div class="post-header">
-                            <h4>{{ $post->title }}</h4>
-							<div class="info">
-                                <a href="{{ route('user.view', ['username' => $post->author->username]) }}">{{ $post->author->username }}</a> @ <span> {{ $post->created_at }}</span>
-                            </div>
-						</div>
-						<div class="post-content">
-							{!! $post->content !!}
-						</div>
-						<hr />
-						@endforeach
-                        {!! $news->render() !!}
-					</div>
+				<div class="panel-body news">
+                    @foreach($news as $post)
+                    <div class="post-header">
+                        <h4>{{ $post->title }}</h4>
+                        <div class="info">
+                            <a href="{{ route('user.view', ['username' => $post->author->username]) }}">{{ $post->author->username }}</a> @ <span> {{ $post->created_at }}</span>
+                        </div>
+                    </div>
+                    <div class="post-content">
+                        {!! $post->content !!}
+                    </div>
+                    <hr />
+                    @endforeach
+                    {!! $news->render() !!}
 				</div>
 			</div>
 		</div>
