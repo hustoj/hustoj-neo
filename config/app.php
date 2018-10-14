bb38694fcbabd2d8bcdab9e533396b78fc57eae0
Sempr <iamsempr@gmail.com>
2018-10-14 14:28:41 +0800

    MOD: support both http and https

--- config/app.php
+++ config/app.php
@@ -53,7 +53,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://www.hustoj.org'),
+    'url' => env('APP_URL', '//www.hustoj.org'),
 
     /*
     |--------------------------------------------------------------------------
