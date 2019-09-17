e56c70007191a5d978445246b8d505d11236af2d
He Jun <freefcw@gmail.com>
2019-09-17 23:04:55 +0800

    feat(config): judger data path can be configured

--- config/hustoj.php
+++ config/hustoj.php
@@ -6,6 +6,6 @@ return [
             'status' => env('JUDGE_SERVICE_OK', true),
         ],
     ],
-    'data_path'            => storage_path('data'),
+    'data_path'            => env('HOJ_DATA_PATH', storage_path('data')),
     'google_analytic_code' => env('GOOGLE_ANALYTIC_CODE', 'UA-6733942-1'),
 ];
