43d4a05155818fe1d065e3ef91cdcb77bb29b8b9
He Jun <freefcw@gmail.com>
2019-09-17 23:04:29 +0800

    feat(problem): hide submit link when user not login

--- resources/views/web/problem/show.blade.php
+++ resources/views/web/problem/show.blade.php
@@ -7,7 +7,10 @@
 
 @section('main')
     @include('web.problem.detail', ['problem' => $problem])
+
+    @if(auth()->check())
     <div class="text-center">
         <a class="btn btn-outline-info btn-lg" href="{{ route('problem.submit', ['problem' => $problem->id]) }}">Submit</a>
     </div>
+    @endif
 @stop
