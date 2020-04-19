fd39796900a408ea83e3a65b8bb1e693eb98650a
He Jun <freefcw@gmail.com>
2020-04-19 20:11:53 +0800

    feat(api): gzip get data api response

--- routes/judge.php
+++ routes/judge.php
@@ -1,5 +1,5 @@
 <?php
 
-app('router')->get('/api/data', 'Judge\ApiController@data');
+app('router')->get('/api/data', 'Judge\ApiController@data')->middleware('zip.response');
 app('router')->post('/api/report', 'Judge\ApiController@report');
 app('router')->post('/api/heartbeat', 'Judge\ApiController@heartbeat');
