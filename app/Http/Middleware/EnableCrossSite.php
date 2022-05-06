86b062acb733b103a582a88b080e3fc2d3f435d3
He Jun <freefcw@gmail.com>
2022-05-07 00:33:17 +0800

    fix cors problem

--- app/Http/Middleware/EnableCrossSite.php
+++ app/Http/Middleware/EnableCrossSite.php
@@ -21,13 +21,12 @@ class EnableCrossSite
         if ($response instanceof BinaryFileResponse) {
             return $response;
         }
-        $response->header('Access-Control-Allow-Origin', '*');
-        // todo: add host name here
-        //        $response->header('Access-Control-Allow-Origin', config('app.url'));
-        $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept');
-        $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
 
-        //        $response->header('Access-Control-Allow-Credentials', 'true');
+        $response->header('Access-Control-Allow-Origin', config('app.url'));
+        $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept, X-REQUESTED-WITH, X-XSRF-TOKEN');
+        $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
+        $response->header('Access-Control-Allow-Credentials', 'true');
+
         return $response;
     }
 }
