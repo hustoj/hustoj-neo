962f482d8b36337329187a0cd3c3ecef42de6cb2
He Jun <freefcw@gmail.com>
2022-08-15 11:51:51 +0800

    optimize user discuss validate code

--- app/Services/Topic/Validator/UserValidator.php
+++ app/Services/Topic/Validator/UserValidator.php
@@ -8,24 +8,35 @@ use Carbon\Carbon;
 
 class UserValidator
 {
-    public function validate(User $user)
+    /**
+     * @throws WebException
+     */
+    public function validate(User $user): void
     {
         $verifiedColdDate = $this->getColdDownDate($user);
-        if ($verifiedColdDate->gt(Carbon::now())) {
+        if ($this->isColdDown($verifiedColdDate)) {
             throw new WebException("You can do it after email verified after {$verifiedColdDate}");
         }
     }
 
-    public function isUserColdDown(User $user)
+    public function isColdDown(Carbon $date): bool
+    {
+        return $date->lt(Carbon::now());
+    }
+
+    public function isUserColdDown(User $user): bool
     {
         $verifiedColdDate = $this->getColdDownDate($user);
 
-        return $verifiedColdDate->lt(Carbon::now());
+        return $this->isColdDown($verifiedColdDate);
     }
 
     public function getColdDownDate(User $user): Carbon
     {
         $userMustVerifiedAfter = config('hustoj.user.topic.verified_after');
+        if ($user->email_verified_at == null) {
+            return Carbon::now()->addHour();
+        }
 
         return $user->email_verified_at->addHours($userMustVerifiedAfter);
     }
